cb85aac2c758283cd07ba671bac2d199b104e7a2
boudekerk <boudekerk@a1771f8f-a78c-4ef5-8dab-c4aa375fc3ed>
2009-10-27 12:37:33 +0000

    [maven-release-plugin]  copy for tag breadcrumb-1.01.01

---
+++ breadcrumb/tags/breadcrumb-1.01.01/src/main/java/org/onehippo/forge/breadcrumb/components/BreadcrumbProvider.java
@@ -0,0 +1,191 @@
+package org.onehippo.forge.breadcrumb.components;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.hippoecm.hst.component.support.bean.BaseHstComponent;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.request.HstRequestContext;
+import org.hippoecm.hst.core.request.ResolvedSiteMapItem;
+import org.hippoecm.hst.core.sitemenu.HstSiteMenu;
+import org.hippoecm.hst.core.sitemenu.HstSiteMenuItem;
+import org.onehippo.forge.breadcrumb.om.Breadcrumb;
+import org.onehippo.forge.breadcrumb.om.BreadcrumbItem;
+
+/**
+ * Provider that can be instantiated within a component to add breadcrumb 
+ * functionalities by composition.   
+ * 
+ * This component creates a basic breadcrumb, but can be easily extended for 
+ * specific, custom needs. 
+ * 
+ * By default, the first part of the breadcrumb is generated from the expanded 
+ * sitemenu items of the menu named 'main' or from multiple menu's as configured
+ * by parameter 'breadcrumb-menus'. The last ("trailing") part of the breadcrumb 
+ * is generated from the resolved sitemap item belonging to the current request.
+ *  
+ * However, the trailing items are only derived when the bean belonging to the 
+ * current resolved sitemap item is a child bean of the bean belonging to the 
+ * deepest expanded sitemenu item. 
+ */
+public class BreadcrumbProvider {
+
+    public static final String ATTRIBUTE_NAME = "breadcrumb";
+    public static final String PARAMETER_MENUS = "breadcrumb-menus";
+    public static final String PARAMETER_SEPARATOR = "breadcrumb-separator";
+    
+    private final BaseHstComponent component;
+
+    /** Constructor */
+    public BreadcrumbProvider(BaseHstComponent component) {
+        super();
+        
+        this.component = component;
+    }
+    
+     /**
+     * Generate the breadcrumb.
+     * 
+     * @param request
+     * @return
+     */
+    public Breadcrumb getBreadcrumb(HstRequest request){
+
+        List<String> siteMenuNames = getSitemenuNames(request);
+
+        // match deepest menu item for multiple configured menus
+        int i = 0; 
+        HstSiteMenuItem deepestMenuItem = null;
+        while ((i < siteMenuNames.size()) && (deepestMenuItem == null)) {
+            HstSiteMenu menu = request.getRequestContext().getHstSiteMenus().getSiteMenu(siteMenuNames.get(i));
+            deepestMenuItem = menu.getDeepestExpandedItem();
+            
+            i++;
+        }
+        
+        // create items if a menu item is found
+        List<BreadcrumbItem> finalList = new ArrayList<BreadcrumbItem>();
+        if (deepestMenuItem != null) {
+            List<BreadcrumbItem> menuBreadcrumbItems = getMenuBreadcrumbItems(request, deepestMenuItem);
+            List<BreadcrumbItem> trailingBreadcrumbItems = getTrailingBreadcrumbItems(request, deepestMenuItem);
+        
+            finalList.addAll(menuBreadcrumbItems);
+            finalList.addAll(trailingBreadcrumbItems);
+        }
+        
+        return new Breadcrumb(finalList, getSeparator(request)); 
+    }
+
+    /**
+     * The multiple site menu names are configured by configuration parameter 
+     * "breadcrumb-menus", defaulting to "main".
+     * 
+     * @param request
+     * @return
+     */
+    protected List<String> getSitemenuNames(HstRequest request){
+        
+        List<String> list = new ArrayList<String>(0);
+        String menuNames = component.getParameter(PARAMETER_MENUS, request);
+        if (menuNames == null) {
+            list.add("main");
+        }
+        else {
+            String[] names = menuNames.split(",");
+            for (int i = 0; i < names.length; i++) {
+                list.add(names[i].trim());
+            }
+        }
+        return list;
+    }
+    
+    /**
+     * Returns the separator string that separates two breadcrumb items.
+     * It is configured by configuration parameter "breadcrumb-separator", 
+     * defaulting to "»". 
+     *
+     * @param request
+     * @return
+     */
+    protected String getSeparator(HstRequest request){
+
+        String separator = component.getParameter(PARAMETER_SEPARATOR, request);
+        return (separator != null) ? separator : "&#187;"; 
+    }
+    
+    /**
+     * Generate the breadcrumb items which correspond to the expanded menu item tree.
+     * 
+     * @param request
+     * @param deepestMenuItem 
+     * @return list of menu breadcrumb items
+     */
+    protected List<BreadcrumbItem> getMenuBreadcrumbItems(HstRequest request, HstSiteMenuItem menuItem){
+        List<BreadcrumbItem> items = new ArrayList<BreadcrumbItem>();
+
+        while (menuItem != null){
+            items.add(getBreadcrumbItem(request, menuItem));
+            menuItem = menuItem.getParentItem();
+        }
+        
+        Collections.reverse(items);
+        
+        return items;
+    }
+    
+    /**
+     * Generate the trailing breadcrumb items. By default, the trailing items 
+     * are derived from the bean structure of the resolved sitemap item of the 
+     * current request. 
+     * 
+     * @param request
+     * @param deepestMenuItem 
+     * @return list of trailing breadcrumb items
+     */
+    protected List<BreadcrumbItem> getTrailingBreadcrumbItems(HstRequest request, HstSiteMenuItem menuItem){
+
+        List<BreadcrumbItem> items = new ArrayList<BreadcrumbItem>();
+
+        ResolvedSiteMapItem ancestorSmi = menuItem.resolveToSiteMapItem(request);
+        ResolvedSiteMapItem currentSmi = request.getRequestContext().getResolvedSiteMapItem();
+        HippoBean ancestorBean = component.getBeanForResolvedSiteMapItem(request, ancestorSmi);
+        HippoBean childBean = component.getBeanForResolvedSiteMapItem(request, currentSmi);
+        
+        if (ancestorBean != null && childBean != null && ancestorBean.isAncestor(childBean)){
+            
+            while (!childBean.isSelf(ancestorBean)){
+                items.add(getBreadcrumbItem(request,childBean));
+                childBean = childBean.getParentBean();
+            }
+            
+            Collections.reverse(items);
+        }
+        
+        return items;
+    }
+    
+    /**
+     * Creates a breadcrumb item belonging to a sitemenu item
+     * 
+     * @param request
+     * @param menuItem
+     * @return breadcrumb item
+     */
+    protected BreadcrumbItem getBreadcrumbItem(HstRequest request, HstSiteMenuItem menuItem){
+        return new BreadcrumbItem(menuItem.getHstLink(),menuItem.getName());
+    }
+    
+    /**
+     * Creates a breadcrumb item belonging to a Hippo bean
+     *  
+     * @param request
+     * @param bean
+     * @return breadcrumb item
+     */
+    protected BreadcrumbItem getBreadcrumbItem(HstRequest request, HippoBean bean){
+        HstRequestContext context = request.getRequestContext();
+        return new BreadcrumbItem(context.getHstLinkCreator().create(bean, context),bean.getName());
+    }
+}
